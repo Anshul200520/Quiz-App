dc31d616f0a490ed0bcbe9b7f45f4cf22e3ac816
Anshul <anshulsidhi143@gmail.com>
2024-06-26 13:23:11 +0530

    first commit

---
+++ app/src/main/java/com/example/quiz_app/questionAnswer.java
@@ -0,0 +1,27 @@
+package com.example.quiz_app;
+
+public class questionAnswer {
+    public static String question[]={
+            "APK stands for -",
+            "Which of the following virtual machine is used by the Android operating system ?",
+            "Which of the following kernel is used in Android ?",
+            "Which type of loop is best known for using an index or counter ?",
+            "In the given option ,which is the Example of System Software ?"
+
+    };
+
+    public static String choice[][]={
+            {"Android Phone Kit","Android Page Kit","Android Package Kit","None of the above"},
+            {"JVM" ,"Dalvik virtual machine","Simple virtual machine","None of the above"},
+            {"MAC","Windows","Linux","Redhat"},
+            {"do-while loop","for(traditional)","for-each","while"},
+            {"windows","Linux","MacOS","All of the above"}
+    };
+    public static String correctanswer[]={
+            "Android Package Kit",
+            "None of the above",
+            "Linux",
+            "for(traditional)",
+            "All of the above"
+    };
+}
